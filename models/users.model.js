a926c9c3aaa73c5b2f7406509e375cb341094273
AlahiraJ <alahirajeffrey@gmail.com>
2021-09-22 11:17:52 +0100

    created app models

---
+++ models/users.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+
+    username : {type: String, require: true, min: 5, max: 20, unique: true},
+    email: {type: String, require: true, unique: true},
+    password : {type: String, require: true, min: 6},
+    profilePicture : {type: String, default: ''},
+    coverPicture : {type: String, default: ''},
+    followers :  {type: Array, default: []},
+    following : {type: Array, default: []},
+    isAdmin : {type : Boolean, default : false},},
+
+{timestamps: true}
+)
+
+module.exports = mongoose.model('User', UserSchema)
